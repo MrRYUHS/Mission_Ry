dd30636b072f7c2a3f0517adf5d48cd7e69dc086
HyeongSoo <rhsrsy123@gmail.com>
2023-11-06 18:04:33 +0900

    명언 5단계

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,60 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public void run(){
         System.out.println("== 명언 앱 ==");
+
+        int lastQuotationId = 0;
+
+        List<Quotation> quotations = new ArrayList<>();
+
         while(true){
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
-            if (cmd.equals("종료")){
-                break;
+            if(cmd.equals("등록")){
+
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                lastQuotationId++;
+                int id = lastQuotationId;
+
+                Quotation quotation = new Quotation(id, content, authorName);
+                quotations.add(quotation);
+
+                System.out.printf("%d번 명언이 등록되었습니다\n", lastQuotationId);
+
+                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
             }
 
-            System.out.printf("입력 받은 명령어 : %s/n", cmd);
+            else if (cmd.equals("종료")){
+                break;
+            }
+            else if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+
+                System.out.println("-----------------------------");
+
+                if(quotations.isEmpty())
+                    System.out.println("등록된 명언이 없습니다");
+
+                Quotation quotation;
+
+                for (int i = quotations.size() - 1; i >= 0; i--) {
+                    quotation = quotations.get(i);
+                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorNmae, quotation.content);
+                }
+            }
         }
     }
 }
---
+++ src/main/java/org/example/Quotation.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Quotation {
+    int id;
+    String content;
+    String authorNmae;
+
+    public Quotation(int id, String content, String authorNmae){
+        this.id = id;
+        this.content = content;
+        this.authorNmae = authorNmae;
+    }
+}
